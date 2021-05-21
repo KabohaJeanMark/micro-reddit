2ddf0f9aa56c28c62fafb83e98d549b9ef6c08f8
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-05-21 14:40:48 +0300

    Add Comment model and associations to User and Post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   has_many :posts
+  has_many :comments
 end
